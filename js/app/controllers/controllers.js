edde8ec6597c58e66936d942245a154bd4596dec
Bryan Baker <bryanpbaker@gmail.com>
2016-05-19 16:49:00 -0500

    hitting walmart api

--- js/app/controllers/controllers.js
+++ js/app/controllers/controllers.js
@@ -4,29 +4,23 @@ toyTrackerApp.controller('GlobalController', ['$scope', function($scope) {
 toyTrackerApp.controller('SearchController', ['$scope', '$http', function($scope, $http) {
 
 	// set the default search term
-	$scope.searchTerm = 'Batman';
+	$scope.searchTerm = 'camera';
 
 	// walmart api public key
 	$scope.publicKey = 'bd3q9g624ym5mrk9ad75ntfw';
-	// api url
-	$scope.apiAddress = 'http://api.walmartlabs.com/v1/search?apiKey=' + $scope.publicKey + '&query=ipod&categoryId=3944&callback=JSON_CALLBACK';
-
 	
+
+	function fetch(){
+
+		$http.jsonp('http://api.walmartlabs.com/v1/search?apiKey=' + $scope.publicKey + '&query=' + $scope.searchTerm + '&categoryId=3944&callback=JSON_CALLBACK')
+		.then(function(response){ 
+			$scope.toys = response.data.items;
+		});
+	}
+
 	// watch the search box and call the api
 	$scope.$watch('searchTerm', function() {
 		fetch();
 	}); 
-
-	function fetch(){
-
-		$http.jsonp($scope.apiAddress)
-			.then(function(response){ 
-
-				$scope.toys = response.data.items;
-
-				console.log($scope.toys);
-
-			});
-	}
 
 }]);
