46a64987bc3660680926692238841159230f0c00
Bryan Baker <bryanpbaker@gmail.com>
2016-05-23 18:51:18 -0500

    Added loader

--- js/app/controllers/controllers.js
+++ js/app/controllers/controllers.js
@@ -8,15 +8,23 @@ toyTrackerApp.controller('SearchController', ['$scope', '$http', function($scope
 
 	// walmart api public key
 	$scope.publicKey = 'bd3q9g624ym5mrk9ad75ntfw';
+
+
+	// loading spinner
+	$scope.loading = true;
 	
 
 	function fetch(){
+		$scope.loading = true;
 
 		$http.jsonp('http://api.walmartlabs.com/v1/search?apiKey=' + $scope.publicKey + '&query=' + $scope.searchTerm + '&categoryId=4171_4172_1156794&numItems=25&sort=bestseller&callback=JSON_CALLBACK')
 		.then(function(response){ 
 			$scope.toys = response.data.items;
 
 			console.log($scope.toys);
+		})
+		.finally(function() {
+			$scope.loading = false;
 		});
 	}
 
