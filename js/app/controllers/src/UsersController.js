12ccd54358e118b3c9205fc7136548eb3ce3e74c
Bryan Baker <bryanpbaker@gmail.com>
2016-06-24 11:31:04 -0500

    Editable profile in dashboard, light styling

--- js/app/controllers/src/UsersController.js
+++ js/app/controllers/src/UsersController.js
@@ -3,12 +3,19 @@ toyTrackerApp.controller('UsersController', ['$scope', '$firebaseObject', 'setPr
 	var usersCtrl = this;
 	var ref = new Firebase('https://toy-tracker-app.firebaseio.com/users/' + authData.uid );
 	$scope.profile = $firebaseObject(ref.child('profile'));
+	$scope.editMode = false;
 
 	
 	$scope.saveProfile = function(){
 		$scope.profile.$save().then(function(response){
 			alert('Your profile has been saved!');
 		});
+
+		$scope.editMode = false;
+	}
+
+	$scope.editProfile = function() {
+		$scope.editMode = true;
 	}
 
 
